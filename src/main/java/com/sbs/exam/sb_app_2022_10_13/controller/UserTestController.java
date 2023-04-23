f24648cf6c1648b6e60ff67ff07f5fddd46f1ad6
redjjang <redbasic@naver.com>
2023-04-23 17:40:47 +0900

    테스트 코딩 추가 안녕하세요. ㅎㅎ

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserTestController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserTestController.java
@@ -1,4 +1,15 @@
 package com.sbs.exam.sb_app_2022_10_13.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class UserTestController {
+  @RequestMapping("/user/home/main")
+  @ResponseBody
+  public String showMain() {
+    return "안녕하세요 ㅎㅎ";
+  }
 }
