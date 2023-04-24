548b1a755aba8e7d75301661e94c37f4addac8e0
redjjang <redbasic@naver.com>
2023-04-25 00:32:49 +0900

    크라켄 추가

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserTestController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserTestController.java
@@ -1,7 +1,7 @@
 package com.sbs.exam.sb_app_2022_10_13.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,6 +10,6 @@ public class UserTestController {
   @RequestMapping("/user/home/main")
   @ResponseBody
   public String showMain() {
-    return "안녕하세요 ㅎㅎ";
+    return "안녕하세요. 크라켄";
   }
 }
